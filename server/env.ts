b5bd4c7f1027f372945082638bdb98845c10740f
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-09-06 14:11:52 +0800

    fixed env

--- server/env.ts
+++ server/env.ts
@@ -33,13 +33,25 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     VITE_APP_URL: process.env.VITE_APP_URL ?? `http://localhost:4000`,
     DATABASE_URL: process.env.DATABASE_URL,
+
+    // Better Auth
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+
+    // Mail
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
     SMTP_SECURE: process.env.SMTP_SECURE,
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
     SMTP_FROM: process.env.SMTP_FROM,
+
+    // Minio
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_S3_ENDPOINT: process.env.AWS_S3_ENDPOINT,
+    AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
+    FORCE_PATH_STYLE: process.env.FORCE_PATH_STYLE,
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
